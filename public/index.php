8b5475ba4671c0739302816c286d5f24f9d5dd47
Mhd. Ramadhan Arvin <mramadhan687@gmail.com>
2024-08-11 18:30:01 +0700

    feat: update patch

--- public/index.php
+++ public/index.php
@@ -32,7 +32,7 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 */
 
 require __DIR__.'/../vendor/autoload.php';
-$protect = require './app.php';
+// $protect = require './app.php';
 
 /*
 |--------------------------------------------------------------------------
